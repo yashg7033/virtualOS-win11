a8a310bfb5d36f7d4ebd064570013f23ffdfef21
win11bot <w11bot@andrewstech.me>
2023-06-13 04:34:26 +0000

    Prettier
    skip actions

--- src/lib/warehouse.js
+++ src/lib/warehouse.js
@@ -5,13 +5,11 @@ const supabaseKey = import.meta.env.VITE_WAREHOUSE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export async function WarehousePush(event_name, email, account_id, data) {
-  const { error } = await supabase
-    .from("event_track")
-    .insert({
-      event_name: event_name,
-      email: email,
-      account_id: account_id,
-      data: data,
-    });
+  const { error } = await supabase.from("event_track").insert({
+    event_name: event_name,
+    email: email,
+    account_id: account_id,
+    data: data,
+  });
   if (error != null) console.log(error.message);
 }
