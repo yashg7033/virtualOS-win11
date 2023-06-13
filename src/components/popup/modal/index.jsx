52bf2517694f223909ac9a7ba4756264b7e7ecba
win11bot <w11bot@andrewstech.me>
2023-06-13 02:03:29 +0000

    Prettier
    skip actions

--- src/components/popup/modal/index.jsx
+++ src/components/popup/modal/index.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 const Modal = (props) => {
   const { isOpen } = props;
-  const theme = useSelector(state => state.setting.person.theme)
+  const theme = useSelector((state) => state.setting.person.theme);
 
   const dispatch = useDispatch();
   const closeModal = () => {
@@ -11,7 +11,7 @@ const Modal = (props) => {
       type: "CLOSE_MODAL",
     });
   };
-  
+
   return (
     <div>
       <ReactModal
@@ -20,14 +20,22 @@ const Modal = (props) => {
         contentLabel="Example Modal"
         className="modalContent "
         overlayClassName="fixed inset-0"
-      //className='d-flex absolute inset-[40px] border-2 border-gray-200 rounded-md outline-none bg-slate-200 overflow-auto'
+        //className='d-flex absolute inset-[40px] border-2 border-gray-200 rounded-md outline-none bg-slate-200 overflow-auto'
       >
-        <div style={{background: theme =='dark' ? 'rgb(255 255 255 / 5%)' : ''}} className="flex flex-col bg-[#eff4f9]">
+        <div
+          style={{ background: theme == "dark" ? "rgb(255 255 255 / 5%)" : "" }}
+          className="flex flex-col bg-[#eff4f9]"
+        >
           <button
             className="self-end flex items-center bg-transparent outline-none border-none px-3 py-2 hover:bg-red-500"
             onClick={closeModal}
           >
-            <img style={{filter:theme =='dark' ? ' invert(1)' : ''}} className="w-[14px]" src="img/icon/ui/close.png" alt="" />
+            <img
+              style={{ filter: theme == "dark" ? " invert(1)" : "" }}
+              className="w-[14px]"
+              src="img/icon/ui/close.png"
+              alt=""
+            />
           </button>
         </div>
         <div className="selectText d-flex overflow-auto min-h-full p-5 pb-9">
