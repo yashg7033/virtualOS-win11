d8abb108b1a9029da2a6ee9302d2e3e6ee9819f1
carosaome <sieunhankiet@gmail.com>
2023-08-04 17:16:47 +0700

    change css

--- src/containers/background/index.jsx
+++ src/containers/background/index.jsx
@@ -163,13 +163,13 @@ export const LockScreen = (props) => {
                     "Run blender ",
                     "Run solid work ",
                   ]}
-                  className="min-w-[70px] inline-block font-bold"
+                  className="w-full text-center inline-block font-bold"
                   typeSpeed={100}
                   loop
                   backSpeed={20}
                   showCursor={false}
                 />
-                <span className="ml-[3px]">on all devices browser</span>
+                <p className="w-full text-center">on all devices browser</p>
               </p>
             </div>
             <div
